87037ed25866c8d3f4a630448e5e9ab46df9c15a
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-10-19 17:35:59 +0900

    Fix require

--- lib/redmine_text_format_selector.rb
+++ lib/redmine_text_format_selector.rb
@@ -1,6 +1,6 @@
 require 'text_format_selector_helper_patch'
 
 module RedmineTextFormatSelector
-  require_dependency 'redmine_text_format_selector/version'
-  require_dependency 'redmine_text_format_selector/plugin'
+  require 'redmine_text_format_selector/version'
+  require 'redmine_text_format_selector/plugin'
 end
